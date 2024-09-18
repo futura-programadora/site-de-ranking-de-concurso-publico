d42c2a61f8a4438ba639eb8a79fb60956b14960e
futura-programadora <162600659+futura-programadora@users.noreply.github.com>
2024-09-18 16:54:13 -0300

    pagina do usuário feita

---
+++ scripts/tela-do-usuario.js
@@ -0,0 +1,8 @@
+function menuUsuario() {
+    var menu = document.querySelector('.menu-usuario');
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
